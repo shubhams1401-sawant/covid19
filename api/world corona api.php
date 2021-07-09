4366c8b610c6282c67c322680e5ed70247616866
shubhams1401-shubham-sawant <shubhams1401@gmail.com>
2021-07-09 11:12:58 +0530

    page id add
    page id add in feedback form

---
+++ api/world corona api.php
@@ -0,0 +1,8 @@
+<?php
+	$data = file_get_contents('https://api.covid19api.com/summary'); //world data
+	// $data = file_get_contents('https://api.covid19india.org/data.json'); // india data
+	$coronadata = json_decode($data);
+
+	echo "<pre>";
+	print_r($coronadata);
+?>	
---
+++ source/js/hacker.js
@@ -0,0 +1,4 @@
+window.oncontextmenu = function() {
+	console.log("Right Click disabled");
+	return false;
+}
